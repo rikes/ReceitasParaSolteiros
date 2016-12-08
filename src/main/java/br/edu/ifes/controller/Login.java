65f4aa3a53fff9d21336582306aa2ddc0f9dad59
amandapersampa <amandapersampa@hotmail.com>
2016-12-08 10:09:08 -0200

    Ajustes nas permissões
    criação dos arquivos de segurança, e de permissão de login.

--- src/main/java/br/edu/ifes/controller/Login.java
+++ src/main/java/br/edu/ifes/controller/Login.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Login {
-    @RequestMapping("/login")
+    @RequestMapping("/index")
     public String index() {
         return "Login Carai!!";
     }
